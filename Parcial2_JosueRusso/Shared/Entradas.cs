b6cc5f54b3de419f6f6c187ac20777e84f0a122e
josueRusso <josuerusso1911@gmail.com>
2023-07-10 19:25:12 -0400

    creando la model

---
+++ Parcial2_JosueRusso/Shared/Entradas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_JosueRusso.Shared
+{
+    public class Entradas
+    {
+        [Key]
+        public int EntradaId { get; set; }
+
+        public string? Fecha { get; set; }
+
+        public string? Concepto { get; set; }
+
+        public string? PesoTotal { get; set; }
+
+        public int ProductoId { get; set; }
+
+        public string? CantidadProducida { get; set; }
+
+
+        [ForeignKey(("EntradasDetalle"))]
+
+        public ICollection<EntradasDetalle> EntradasDetalles { get; set; } = new List<EntradasDetalle>();
+
+    }
+
+
+    public class EntradasDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+
+        public int EntradaId { get; set; }
+
+        public int ProductoId { get; set; }
+
+        public string? CantidadUtilizada { get; set; }
+
+    }
+}
